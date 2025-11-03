0c5608778b4501bda41f59f8aaffd11dd20d6b9b
Vipul <vipullabkh@gmail.com>
2025-11-03 15:17:00 +0530

    mul.java updated

--- mul.java
+++ mul.java
@@ -1,3 +1,8 @@
 public class mul {
-    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int product = a * b;
+        System.out.println("The product is : " + product);
+    }
 }

8411d52336c873dba604d3160b02aca52555bb34
Vipul <vipullabkh@gmail.com>
2025-11-03 15:06:57 +0530

    2 java files created.

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class sub {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = a - b;
+        System.out.println("The difference is : " + difference);
+    }
+}
